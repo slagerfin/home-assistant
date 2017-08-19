3158703ceded36fa600dd9ad9a460c4fc67c9159
Samuli Lager <samuli.lager@dev.mail.kapsi.fi>
2017-08-19 21:25:24 +0300

    Improve ruuvitag support
    * Support naming tags in config
    * Throttle tag polling
    * Use mac address as a tag identifier

--- homeassistant/components/sensor/ruuvitag.py
+++ homeassistant/components/sensor/ruuvitag.py
@@ -1,10 +1,21 @@
+import logging
+from datetime import timedelta
+
 from homeassistant.const import TEMP_CELSIUS
+from homeassistant.util import Throttle
 from homeassistant.helpers.entity import Entity
 
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
 from ruuvitag_sensor.ruuvitag import RuuviTag
 
 REQUIREMENTS = ['ruuvitag-sensor==0.8.0']
+
+
+class ThrottledRuuviTag(RuuviTag):
+    @Throttle(min_time=timedelta(seconds=60))
+    def update(self, *args, **kwargs):
+        return super().update(*args, **kwargs)
+
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Setup the sensor platform."""
@@ -17,20 +28,25 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     sensors = []
     for mac, data in ruuvitags.items():
         identifier = data.pop('identifier')
+        try:
+            name = config['sensors'][mac]['name']
+        except ValueError:
+            name = identifier
         for data_type, initial_state in data.items():
-            sensors.append(SENSOR_TYPES[data_type](mac, identifier))
+            sensors.append(SENSOR_TYPES[data_type](mac, name))
     add_devices(sensors)
 
 
 class BaseRuuviTagSensor(Entity):
     sensors = {}
-    def __init__(self, mac, identifier):
+    def __init__(self, mac, name):
         """Initialize the sensor."""
-        if not identifier in self.sensors:
-            sensor = RuuviTag(mac)
+        self._mac = mac
+        self._name = name
+        if not mac in self.sensors:
+            sensor = ThrottledRuuviTag(mac)
             sensor.update()
-            self.sensors[identifier] = sensor
-        self._identifier = identifier
+            self.sensors[mac] = sensor
 
     @property
     def sensor_type(self):
@@ -38,12 +54,12 @@ class BaseRuuviTagSensor(Entity):
 
     @property
     def sensor(self):
-        return self.sensors[self._identifier]
+        return self.sensors[self._mac]
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return self.sensor_type.title()
+        return '{} {}'.format(self._name, self.sensor_type.title())
 
     @property
     def state(self):
